fdd1eef6a88e4708ad6b120eea4a0ca12aef4419
BolachinhaAmericana <duarte.tz.valente@gmail.com>
2022-12-03 17:04:43 +0000

    Updated Installer so It is no longer necessary to run taxonkit with as root

--- taxonKitInstaller(Fora do Docker).sh
+++ taxonKitInstaller(Fora do Docker).sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
+mkdir ./Downloading...
+cd ./Downloading...
 
-# This script must be run with root privileges, if the user is not root. It will simply exit and do nothing.
-# This if statement is merely checking whether or not the script is running with root privileges.
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-mkdir ./download
-cd download
-mkdir ./1
-cd 1
+#Download and extract taxonkit
 wget https://github.com/shenwei356/taxonkit/releases/download/v0.14.0/taxonkit_linux_amd64.tar.gz
-tar -zxvf *.tar.gz 
-cp taxonkit /usr/local/bin/
+tar -zxvf *.tar.gz
+
+#install taxonkit without root 
 mkdir -p $HOME/bin/; cp taxonkit $HOME/bin/
-cd ..
-mkdir ./2
-cd 2
+
+#Checking if taxonkit is installed
+taxonkit version
+taxonkit genautocomplete
+
+#Download and extract database
 wget http://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
 tar -zxvf taxdump.tar.gz
-mv *.dmp /root/.taxonkit
+
+#setting up env and installing database
+mkdir -p $HOME/.taxonkit
+cp names.dmp nodes.dmp merged.dmp delnodes.dmp $HOME/.taxonkit
+
+#deleting junk files
 cd ..
-cd ..
-rm -r download
+rm -r ./Downloading...
