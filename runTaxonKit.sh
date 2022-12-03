fb400c877a17f74da1e9f7c66eb5e1e5aa02d2c6
BolachinhaAmericana <duarte.tz.valente@gmail.com>
2022-12-03 17:07:16 +0000

    Running this script will no longer maintain the Output.txt file. It was just unnecessary

--- runTaxonKit.sh
+++ runTaxonKit.sh
@@ -1,12 +1,6 @@
-#!/bin/bash
-
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
 taxonkit list --ids 9443 -nr --indent "" > Output.txt
 grep -r "species" ./Output.txt > 1.txt
 grep -v "subspecies" ./1.txt > Output.txt
 sed 's/\s.*$//' Output.txt  > SpeciesIdsFromRank.txt
 rm 1.txt
+rm Output.txt
