74a6dafe30a5d1660bd65d235dcac274ba0a91dd
Bolachinha Americana <duarte.tz.valente@gmail.com>
2022-12-04 16:20:31 +0000

    Rewritten code on python

--- runTaxonKit.sh
+++
@@ -1,6 +0,0 @@
-taxonkit list --ids 9443 -nr --indent "" > Output.txt
-grep -r "species" ./Output.txt > 1.txt
-grep -v "subspecies" ./1.txt > Output.txt
-sed 's/\s.*$//' Output.txt  > SpeciesIdsFromRank.txt
-rm 1.txt
-rm Output.txt
