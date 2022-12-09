9931778d773e4104f6716267b5b473acc6407805
BolachinhaAmericana <duarte.tz.valente@gmail.com>
2022-12-09 22:23:32 +0000

    Changed file so it downloads all geneLists from ScientificNames_list.txt species

--- scripts/getSpeciesGeneList.py
+++ scripts/getSpeciesGeneList.py
@@ -2,7 +2,7 @@ from Bio import Entrez
 import os
 import sys
 
-Entrez.email = "guilherme.vcc.silva@gmail.com" 
+Entrez.email = "noWarningPls@gmail.com" 
 
 def eSearch(db, term):
     '''
@@ -23,43 +23,52 @@ def eSearch(db, term):
     eSearchResult = Entrez.read(eSearch)
     return eSearchResult
 
-term= 'Panthera leo' #One specie at a Time
 
-search_results= eSearch("Gene", term)
 
-acc_list = search_results["IdList"]
-count = int(search_results["Count"])
-webenv = search_results["WebEnv"]
-query_key = search_results["QueryKey"]
-batch_size = 9999
+with open('ScientificNames_list.txt', 'r') as List:
+    nameList = [nameList.rstrip() for nameList in List]
 
-os.system("rm rawFile.txt")
+nameList= list(set(nameList)) 
+nameList.remove("")
 
-for start in range(0, count, batch_size):
-    end = count
-    print("Going to download record %i to %i" % (start + 1, end))
-    fetch_handle = Entrez.efetch(
-        db="Gene",
-        rettype="gb",
-        retmode="text",
-        retstart=start,
-        retmax=batch_size,
-        webenv=webenv,
-        query_key=query_key,
-        idtype="acc",
-        )
-    data = fetch_handle.read()
+for name in nameList:
 
-    myfile= open('rawFile.txt', 'a')
-    myfile.writelines(data)
-    myfile.close()
+    term= (f'{name}') #One specie at a Time
 
-    fetch_handle.close()
+    search_results= eSearch("Gene", term)
 
-os.system("grep -v ':' rawFile.txt > loading.txt")
-os.system("grep '\.' loading.txt > rawFile.txt")
-os.system("grep -v '\[' rawFile.txt > loading.txt")
-os.system("sed 's/^.*\ //' loading.txt > GeneList.txt")
-os.system("rm loading.txt")
-os.system("rm rawFile.txt")
-os.rename('GeneList.txt', '{}_GeneList'.format(term))
+    acc_list = search_results["IdList"]
+    count = int(search_results["Count"])
+    webenv = search_results["WebEnv"]
+    query_key = search_results["QueryKey"]
+    batch_size = 9999
+
+
+    os.system("mkdir ./GeneLists")
+    for start in range(0, count, batch_size):
+        end = count
+        print("Going to download record %i to %i - %i" % (start + 1, end, term))
+        fetch_handle = Entrez.efetch(
+            db="Gene",
+            rettype="gb",
+            retmode="text",
+            retstart=start,
+            retmax=batch_size,
+            webenv=webenv,
+            query_key=query_key,
+            idtype="acc",
+            )
+        data = fetch_handle.read()
+
+        myfile= open('rawFile.txt', 'a')
+        myfile.writelines(data)
+        myfile.close()
+
+        fetch_handle.close()
+
+    os.system("grep -v ':' rawFile.txt > loading.txt")
+    os.system("grep '\.' loading.txt > rawFile.txt")
+    os.system("grep -v '\[' rawFile.txt > loading.txt")
+    os.system("sed 's/^.*\ //' loading.txt > GeneList.txt")
+    os.system("rm loading.txt")
+    os.rename('GeneList.txt', './GeneLists/{}_GeneList'.format(term))
