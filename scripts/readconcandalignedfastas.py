90f00275ec3eaae3b70fba7154890638a6599116
@Sepay1 <pintorodrigo28@gmail.com>
2022-12-28 17:04:00 +0000

    Added the part where the names of the sequences will be modified to the name of the species, after the concatenation part

--- scripts/readconcandalignedfastas.py
+++ scripts/readconcandalignedfastas.py
@@ -12,4 +12,16 @@ for file in os.listdir(pasta):
     with open(fastafile, 'r') as f:
         conca+= f.read()
 
-print(conca)
+
+#Alterar o nome do fasta concatenado
+listaNomes= ["X1", "X2"]
+contador = 0
+with open("test.fasta", "r") as r:
+    with open ("newtxt","w") as f:
+        for line in r:
+            if line.startswith(">"):
+                f.write(">"+ listaNomes[contador] +"\n")
+                contador +=1
+            else:
+                f.write(line)
+        f.close()
