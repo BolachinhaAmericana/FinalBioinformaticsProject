2c9bbcee8e07236f0ed57d1e14b4ab4bb33626cb
@Sepay1 <pintorodrigo28@gmail.com>
2022-12-28 15:56:01 +0000

    Little modification to the jmodel2.py about the fasta input. Read the last line of the output file and see if the right model has +I derivation

--- scripts/jmodel2.py
+++ scripts/jmodel2.py
@@ -9,7 +9,19 @@ import subprocess
 #-s number of substituion 11 to use all models defined by jmodeltest documentation
 #-v model averaging and parameter importance
 #-t for choose what the base tree. Maximum likelihoodL in this example
+#fasta = ficheiro fasta concatenado recebido recebido
 
-subprocess.run('jmodeltest -d AK_s7rp_aln.fasta -S BEST -tr 4  -p  -AIC -s 11 -i -v -t ML -o teste2', shell = True)
+subprocess.run(f'jmodeltest -d {fasta} -tr 4  -p  -AIC -s 11 -i -v -t ML -o teste2', shell = True)
+
+with open("teste2") as file:
+    for line in file:
+        pass
+    last_line = line
+
+if "+i" in last_line:
+    print("Executar comando raxML com derivação +I")
+else:
+    print("Executar comando raxML sem derivação +I")
 
 
+subprocess.run("rm teste2", shell = True)
