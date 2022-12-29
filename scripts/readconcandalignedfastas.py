67d156a2f22394901fe43ddd5e3c56904631bc7e
@Sepay1 <pintorodrigo28@gmail.com>
2022-12-29 16:06:59 +0000

    Improved the fastaFinal function, new funcion that will get a list of species names and started the alignment procedure

--- scripts/readconcandalignedfastas.py
+++ scripts/readconcandalignedfastas.py
@@ -5,23 +5,40 @@ import subprocess
 
 #This script will receive 2 inputs that will be one folder with all the fasta files and the second is the list of species names.
 
+#alinhar fastas
+
+folder= 'testefasta'
+outputdir = 'alignedfastas'
+
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
+fastaFiles = [f for f in os.listdir(folder)]
+
+for fasta in fastaFiles:
+    subprocess.run(["mafft",fasta], cwd = folder)
 
 #concatenation of the fasta files
-conca = ''
-for file in os.listdir(pasta):
-    with open(fastafile, 'r') as f:
-        conca+= f.read()
+#conca = ''
+#for file in os.listdir(pasta):
+  #     with open(fastafile, 'r') as f:
+   #         conca+= f.read()
 
 
-#Alterar o nome do fasta concatenado
-listaNomes= ["X1", "X2"]
-contador = 0
-with open("test.fasta", "r") as r:
-    with open ("newtxt","w") as f:
+def getlistofNames(namesFile):
+    listaNomes= []
+    with open (f"{namesfile}", "r") as r:
         for line in r:
-            if line.startswith(">"):
-                f.write(">"+ listaNomes[contador] +"\n")
-                contador +=1
-            else:
-                f.write(line)
-        f.close()
+            listaNomes.append(line.strip())
+        return listaNomes
+
+def fastaFinal(listNames,concatenatedFasta):
+    contador = 0
+    with open(f"{concatenatedFasta}", "r") as r:
+        with open ("concatenatedFinal.fasta","w") as f:
+            for line in r:
+                if line.startswith(">"):
+                    f.write(">"+ listaNomes[contador] +"\n")
+                    contador +=1
+                else:
+                    f.write(line)
+            f.close()
