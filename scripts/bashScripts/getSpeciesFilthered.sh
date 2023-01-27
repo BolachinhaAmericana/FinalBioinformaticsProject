c028f4da6b09bb98ec7162d7179c718bf0026e6e
GuilhermeVCCdaSilva <guilherme.vcc.silva@gmail.com>
2023-01-27 16:04:52 +0000

    remove bash files

--- scripts/bashScripts/getSpeciesFilthered.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Made by Valente
-grep 'species' test.xml > list.xml
-grep -v 'subspecies' list.xml > loading.xml
-
--- scripts/bashScripts/removeDump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Made by Valente
-rm list.xml
-rm test.xml
-rm loading.xml
-rm test1.xml
-rm eFetch.xml
