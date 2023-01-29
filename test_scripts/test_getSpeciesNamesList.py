5d3f2fe13b3729837cae9a0097cc81ecc11c0934
marine.fournier2002 <marine.fournier2002@gmail.com>
2023-01-29 01:13:04 +0000

    update test_getSpeciesRankName.py

--- test_scripts/test_getSpeciesNamesList.py
+++ test_scripts/test_getSpeciesNamesList.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python3
-
-
-########################################### Test_Version ################################################################
-
 import os
 import pytest
-from scripts.getSpeciesNamesList import downloadScientificNamesList
-import scripts.essentials
-
+from scripts.getSpeciesNamesList import scientific_names_list_downloader
 
 def test_scientific_names_list_downloader():
     # test the function with a known input
-    rank_name = 'primates'
-    downloadScientificNamesList(rank_name)
+    rank_name = 'Passeridae'
+    scientific_names_list_downloader(rank_name)
     # check if the output file exists
     assert os.path.exists('ScientificNames_list.txt')
     # check if the file is not empty
     assert os.stat('ScientificNames_list.txt').st_size != 0
 
-def test_scientific_names_list_downloader_without_input():
-    # test the function with no input
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        downloadScientificNamesList()
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 'No input was give. This program takes a species taxonomy level (ex: primates).'
