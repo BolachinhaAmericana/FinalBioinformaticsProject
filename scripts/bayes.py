d28175171c2f885838a962a3626ff494a7252714
matilde machado <matmachado2001@gmail.com>
2023-01-29 16:38:50 +0000

    Replace bayes.py

--- scripts/bayes.py
+++ scripts/bayes.py
@@ -1,16 +1,13 @@
 import os
 
-# Set the input and output directories
-input_dir = "/path/to/input/fasta/files"
-output_dir = "/path/to/output/tree/files"
-
-# Loop through all fasta files in the input directory
-for file in os.listdir(input_dir):
-    if file.endswith(".fasta"):
-        # Get the file name without the extension
-        file_name = os.path.splitext(file)[0]
-        # Run Mr bayes on the fasta file
-        os.system(f"mrbayes -s {input_dir}/{file} -n {file_name} -o {output_dir}/{file_name}.tre")
-
-# Print a message to indicate the script has completed
-print("Mr bayes tree creation complete!")
+os.system("seqmagick convert --output-format nexus --alphabet dna concat.fasta concat.nex")
+with open("concat.nex", "a") as f:
+    f.write(""" 
+begin mrbayes;
+set autoclose=yes;
+mcmc ngen=1000000 printfreq=500 samplefreq=500 diagnfreq=5000 nchains=4 savebrlens=yes;
+mcmc;
+smut filename=Myrun01;
+end;
+""")
+os.system("mb concat.nex")
