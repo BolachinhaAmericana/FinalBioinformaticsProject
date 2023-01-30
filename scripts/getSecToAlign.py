066b885b13a0656361e27953d00d9e942983ed25
GuilhermeVCCdaSilva <guilherme.vcc.silva@gmail.com>
2023-01-30 02:46:05 +0000

    update getSecToAlign

--- scripts/getSecToAlign.py
+++ scripts/getSecToAlign.py
@@ -6,16 +6,6 @@ import shutil
 import os 
 
 def createSquences_Fasta_Dir(directory):
-    '''
-    What for:
-        This function is used to create a directory to store FASTA files.
-
-    Arguments/Vars:
-        directory: Name of the directory to be created.
-
-    Returns:
-        pathToFastaDir: Path of the created directory. 
-    '''
     while True:
         try:
             os.mkdir(directory)
@@ -26,28 +16,6 @@ def createSquences_Fasta_Dir(directory):
 
 
 def getFastas(pathToFastaDir,db):
-    '''
-    What for:
-        This function downloads FASTA sequences from a given database using gene names and scientific names.
-         
-    Arguments:
-        pathToFastaDir: Directory where the FASTA files will be stored
-        db: Database to download the sequences from.
-    
-    Vars:
-        gene_file: File object representing the file containing the gene names.
-        scientificNames_file: File object representing the file containing the scientific names.
-        term: Represents the search term to be used in the entrez_search function.
-        eSearchResults: Dictionary containing the results of the entrez_search function.
-        webEnv: Represents the web environment of the search results.
-        queryKey: Represents the query key of the search results.
-        data: Data returned by the entrez_fetch function.
-        fastaPath: Represents the path of the FASTA.
-        fastaFile: Represents the FASTA file.
-
-    Returns:
-
-    '''
     gene_file  = open(f"FiltredGeneNames_list.txt", "r")
     for gene in gene_file:
         scientificNames_file = open("FiltredScientificNames_list.txt", "r")
@@ -77,11 +45,4 @@ def getFastas(pathToFastaDir,db):
 if __name__ == '__main__':
     pathToFastaDir = createSquences_Fasta_Dir(directory = 'Squences_Fasta')
     getFastas(pathToFastaDir,db='Nucleotide')
-
-
-
-
-
-
-
 
