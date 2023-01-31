645d3f02bf5b8d501e3fbc0efff18294c817648a
GuilhermeVCCdaSilva <guilherme.vcc.silva@gmail.com>
2023-01-31 18:36:47 +0000

    Final Updates -x to a rapid Execution

--- scripts/getML_Tree.py
+++ scripts/getML_Tree.py
@@ -14,11 +14,11 @@ def getBestModel(fasta):
 
 def raxMLExecutor(lastLine):
     if "+I" in lastLine:
-        subprocess.run("raxmlHPC -m GTRCATI -p 1234567 -b 1234567 -# autoFC -s concat.fasta -n nhk", shell = True)
+        subprocess.run("raxmlHPC -m GTRCATI -p 1234567 -x 1234567 -# autoFC -s concat.fasta -n nwk", shell = True)
     else:
-        subprocess.run("raxmlHPC -m GTRCAT -p 1234567 -b 1234567 -# autoFC -s concat.fasta -n nhk", shell = True)
+        subprocess.run("raxmlHPC -m GTRCAT -p 1234567 -x 1234567 -# autoFC -s concat.fasta -n nwk", shell = True)
     subprocess.run("rm -r model*", shell = True)
-    subprocess.run("RAxML_info.nhk", shell = True) 
+    subprocess.run("RAxML_info.nwk", shell = True) 
 
 if __name__ == "__main__":
     lastLine = ""
