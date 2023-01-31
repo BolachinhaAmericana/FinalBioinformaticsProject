be3968a8246d5ea30afd4b0a92da1630ed02f5b8
GuilhermeVCCdaSilva <guilherme.vcc.silva@gmail.com>
2023-01-31 18:35:37 +0000

    Final Updates

--- scripts/getBayes_Tree.py
+++ scripts/getBayes_Tree.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-import os
-
-os.system("seqmagick convert --output-format nexus --alphabet dna concat.fasta concat.nex")
+import subprocess
+subprocess.run("seqmagick convert --output-format nexus --alphabet dna concat.fasta concat.nex", shell = True)
 with open("concat.nex", "a") as f:
     f.write(""" 
 begin mrbayes;
 set autoclose=yes;
-mcmc ngen=1000 printfreq=500 samplefreq=500 diagnfreq=5000 nchains=4 savebrlens=yes filename=MyRun01;
+mcmc ngen=1000 printfreq=500 samplefreq=500 diagnfreq=5000 nchains=4 savebrlens=yes filename=MyBayes;
 mcmc;
 sump;
-sumt;
-output format=newick;
+sumt conformat=simple contype=halfcompat;
 end;
 """)
 
-os.system("mb concat.nex")
-os.system("mv MyRun01.con.tre MyBayes.tre")
-os.system("rm -r MyRun*")
+subprocess.run("mb concat.nex", shell = True)
