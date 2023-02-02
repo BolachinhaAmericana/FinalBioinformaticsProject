54831029d9d52cfcbbe6bc33a7c39ebc3cda7c41
alvesgoncas2014 <alvesgoncas2014@gmail.com>
2023-02-02 19:45:20 +0000

    Docstring ne :)

--- scripts/getML_Tree.py
+++ scripts/getML_Tree.py
@@ -4,7 +4,21 @@ import subprocess
 
 
 def getBestModel(fasta):
+    '''
+    What for:
+        Runs modeltest-ng on a fasta file to determine the best model of evolution.
 
+    Arguments:
+        fasta: Path to the fasta file to run modeltest-ng on.
+    
+    Vars:
+        content: list that contains the lines of the file "Model.txt".
+        lastLine: Variable that is used to store the last line of the file "Model.txt".
+
+    Returns:
+       lastLine
+
+    '''
     subprocess.run(f'modeltest-ng -d nt -i {fasta} -o model -p 4 -f e -h i -s 11 > model.txt', shell = True)
 
     with open("model.txt") as file:
@@ -13,6 +27,18 @@ def getBestModel(fasta):
     return lastLine
 
 def raxMLExecutor(lastLine):
+    '''
+    What for:
+        This function is used to run RaxML on a given FASTA file.
+
+    Arguments/Vars:
+        fasta: Path of the FASTA file to be used as input for RaxML.
+        lastLine: The last line of the file.
+        
+    Returns:
+       None
+
+    '''
     if "+I" in lastLine:
         subprocess.run("raxmlHPC -m GTRCATI -p 1234567 -x 1234567 -# autoFC -s concat.fasta -n nwk", shell = True)
     else:
