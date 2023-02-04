92f579e193f8aed533b999a4a49720f8c01da1f2
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-02-04 01:26:37 +0000

    merged and exec test done

--- scripts/get_toy_tree.py
+++ scripts/get_toy_tree.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python
-import toytree # Uma biblioteca para o desenho gráfico de árvores           
-import toyplot.pdf
+'''Tree design'''
+
 import sys
+import toytree # Uma biblioteca para o desenho gráfico de árvores
+import toyplot.pdf
+from essentials import get_user_arguments
 
-def get_user_input(MLTree, MrBayes):
-    MLTree = sys.argv[1] 
-    MrBayes = sys.argv[2]
-    return MLTree, MrBayes
-
-def get_user_args(specieName):
-    specieName = sys.argv[3]
-    specieName = specieName.replace(" ", "_")
-    print(specieName) 
-    return specieName
-
-
-def oppen_tree_file(treeFile):
-    with open(treeFile,'r') as text:
-        treeFile = text.read()
-    tre = toytree.tree(treeFile) 
+def open_tree_file(tree_file):
+    '''Opens tree file'''
+    with open(tree_file,'r', encoding='utf8') as text:
+        tree_file = text.read()
+    tre = toytree.tree(tree_file)
     print(tre)
     return tre
 
-def style_MLtree(specieName,tre):
+def style_ml_tree(specie_name,tre):
+    '''sets tree style'''
      # Uma lista com as diversas cores para os nomes das espécies da árvores
-    colorlist = ["darkcyan" if specieName in t else "darkorange" for t in tre.get_tip_labels()]
+    color_list = ["darkcyan" if specie_name in t else "darkorange" for t in tre.get_tip_labels()]
     # Define os estilos a utilizar
-    mystyle = {
+    my_style = {
         "edge_type": 'p',
         "edge_style": {
             "stroke": toytree.colors[2],
@@ -35,7 +28,7 @@ def style_MLtree(specieName,tre):
             "stroke-width": 2.5,
         },
         "tip_labels_align": True,
-        "tip_labels_colors" : colorlist,
+        "tip_labels_colors" : color_list,
         "tip_labels_style": {
             "font-size": "10px",
         },
@@ -44,23 +37,21 @@ def style_MLtree(specieName,tre):
         "node_colors": toytree.colors[2],
     }
     # Guarda os valores que retornam do desenho da arvore em canvas, axes, mark
-    mlCanvas ,axes, mark = tre.draw(height=900,**mystyle)
-    return mlCanvas
-
-def style_MBtree(specieName,tre):
+    ml_canvas ,axes, mark = tre.draw(height=900,**my_style)
+    return ml_canvas
+def mb_tree_styler(specie_name, tre):
+    ''' sets the style for the mb tree'''
     # Uma lista com as diversas cores para os nomes das espécies da árvores
-    colorlist = ["darkcyan" if specieName in t else "indigo" for t in tre.get_tip_labels()]
-
-
+    color_list = ["darkcyan" if specie_name in t else "indigo" for t in tre.get_tip_labels()]
     # Define os estilos a utilizar
-    mystyle = {
+    my_style = {
         "edge_type": 'p',
         "edge_style": {
             "stroke": toytree.colors[2],
             "stroke-width": 2.5,
         },
         "tip_labels_align": True,
-        "tip_labels_colors": colorlist,
+        "tip_labels_colors": color_list,
         "tip_labels_style": {
             "font-size": "10px"
         },
@@ -70,23 +61,20 @@ def style_MBtree(specieName,tre):
     }
 
     # Guarda os valores que retornam do desenho da arvore em canvas, axes, mark
-    mbCanvas, axes, mark = tre.draw(height=900,**mystyle)
-    return mbCanvas
+    mb_canvas, axes, mark = tre.draw(height=900,**my_style)
+    return mb_canvas
 
-def write_to_pdf(mlCanvas,mbCanvas):
-    toyplot.pdf.render(mlCanvas, "tree-plot_bootstrap_ML.pdf")
-    toyplot.pdf.render(mbCanvas, "tree-plot_MB.pdf")
+def write_to_pdf(ml_canvas,mb_canvas):
+    '''writes input to pdf file'''
+    toyplot.pdf.render(ml_canvas, "tree-plot_bootstrap_ML.pdf")
+    toyplot.pdf.render(mb_canvas , "tree-plot_MB.pdf")
 
 if __name__ == '__main__':
-    MLTree, MrBayes = get_user_input(MLTree='', MrBayes='')
-    specieName = get_user_args(specieName='')   
-    tre = oppen_tree_file(treeFile=MLTree)
-    mlCanvas = style_MLtree(specieName,tre)
-    tre = oppen_tree_file(treeFile=MrBayes)
-    mbCanvas = style_MBtree(specieName,tre)
-    write_to_pdf(mlCanvas,mbCanvas)
+    ml_tree, mr_bayes, specie = get_user_arguments(3)
+    specie = specie.replace(" ", "_")
 
-
-
-
-
+    tree = open_tree_file(tree_file=ml_tree)
+    max_likelyhood_canvas = style_ml_tree(specie,tree)
+    tree = open_tree_file(tree_file=mr_bayes)
+    mr_bayes_canvas = mb_tree_styler(specie,tree)
+    write_to_pdf(max_likelyhood_canvas,mr_bayes_canvas)
