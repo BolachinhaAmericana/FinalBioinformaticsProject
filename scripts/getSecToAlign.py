a90f6dc6dc75f52d0ebd055352854956fff73797
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-02-04 01:25:54 +0000

    merged and exec test done

--- scripts/getSecToAlign.py
+++ scripts/getSecToAlign.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
+''' Takes one gene at a time and downloads all species with said gene.'''
 
-#Made by Marine and Silva
-from essentials import entrez_search, entrez_fetch
 import shutil
-import os 
+import os
+from essentials import entrez_search, entrez_fetch
 
-def createSquences_Fasta_Dir(directory):
+def fasta_dir_creator(directory):
+    '''Created a file for the fastas to go to.'''
     while True:
         try:
             os.mkdir(directory)
-            pathToFastaDir = directory
-            return pathToFastaDir
+            fasta_dir_path = directory
+            return fasta_dir_path
         except FileExistsError:
-            shutil.rmtree('Squences_Fasta') 
+            shutil.rmtree('Squences_Fasta')
 
-
-def getFastas(pathToFastaDir,db):
-    gene_file  = open(f"FiltredGeneNames_list.txt", "r")
+def fasta_downloader(fasta_dir_path):
+    '''
+    takes one gene at a time from FiltredGeneNames list file.
+    downloads a fasta file for each gene with the information about said gene
+        for all species in FiltheredScientificNames list file.
+    '''
+    gene_file  = open("FiltredGeneNames_list.txt", "r", encoding='utf8')
     for gene in gene_file:
-        scientificNames_file = open("FiltredScientificNames_list.txt", "r")
+        names_file = open("FiltredScientificNames_list.txt", "r", encoding='utf8')
         print(gene)
-        fastaPath = f'{gene}.fasta'.replace("\n", "")
-        for scientificName in scientificNames_file:
+        fasta_path = f'{gene}.fasta'.replace("\n", "")
+        for name in names_file:
             while True:
                 try:
-                    term = f'{scientificName}[Organism] AND {gene}[gene]'
-                    eSearchResults = entrez_search(db,term)
-                    webEnv = eSearchResults["WebEnv"]
-                    queryKey = eSearchResults["QueryKey"]
-                    print(f"Downloading record {scientificName} of {gene}")
-                    data = entrez_fetch(db,webEnv,queryKey)
-                    fastaFile = open(fastaPath, "a")
-                    fastaFile.writelines("%s\n" %data)
-                    fastaFile.close() 
+                    term = f'{name}[Organism] AND {gene}[gene]'
+                    search_result = entrez_search('Nucleotide', term)
+                    web_env = search_result["WebEnv"]
+                    query_key = search_result["QueryKey"]
+
+                    print(f"Downloading record {name} of {gene}")
+
+                    data = entrez_fetch('Nucleotide', web_env,query_key)
+
+                    fasta_file = open(fasta_path, "a", encoding='utf8')
+                    fasta_file.writelines(f"{data}\n")
+                    fasta_file.close()
                     break
-                except:
-                    print('Error while downloading. Trying again...')                       
-        shutil.copy2(fastaPath,pathToFastaDir)
-        os.remove(fastaPath)        
+                except Exception:
+                    print('Error while downloading. Trying again...')
+        shutil.copy2(fasta_path,fasta_dir_path)
+        os.remove(fasta_path)
     gene_file.close()
-    scientificNames_file.close()
-            
+    names_file.close()
 
 if __name__ == '__main__':
-    pathToFastaDir = createSquences_Fasta_Dir(directory = 'Squences_Fasta')
-    getFastas(pathToFastaDir,db='Nucleotide')
-
+    WORKING_PATH = fasta_dir_creator(directory = 'Squences_Fasta')
+    fasta_downloader(WORKING_PATH)
