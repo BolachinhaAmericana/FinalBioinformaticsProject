85dc17e07097512f87770df54ed002d9a4cb7637
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-02-04 01:24:00 +0000

    pep8 merged and exec test done

--- scripts/getBayes_Tree.py
+++ scripts/getBayes_Tree.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+'''mr bayes'''
 import subprocess
-subprocess.run("seqmagick convert --output-format nexus --alphabet dna concat.fasta concat.nex", shell = True)
-with open("concat.nex", "a") as f:
-    f.write(""" 
+subprocess.run("seqmagick convert --output-format nexus --alphabet dna concat.fasta concat.nex", shell = True, check = False)
+
+with open("concat.nex", "a", encoding = 'utf8') as doc:
+    doc.write("""
 begin mrbayes;
 set autoclose=yes;
 mcmc ngen=100000 printfreq=500 samplefreq=500 diagnfreq=5000 nchains=4 savebrlens=yes filename=MyBayes;
@@ -11,5 +13,4 @@ sump;
 sumt conformat=simple contype=halfcompat;
 end;
 """)
-
-subprocess.run("mb concat.nex", shell = True)
+subprocess.run("mb concat.nex", shell = True, check = False)
