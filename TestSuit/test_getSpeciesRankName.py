a7aa57b27a2e75bd0cab1bc762c93921fedc94bc
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-02-04 01:33:01 +0000

    added TestSuit to pep8 func new name

--- TestSuit/test_getSpeciesRankName.py
+++ TestSuit/test_getSpeciesRankName.py
@@ -3,13 +3,7 @@
 
 ########################################### Test_Version ################################################################
 import sys
-from scripts.getSpeciesRankName import get_user_arguments, get_taxon_name,error_handle_taxon_name
-
-def test_get_user_arguments():
-    sys.argv = ["getSpeciesRankName.py","Passer domesticus", "family"]
-    scientific_name, taxonomy_rank = get_user_arguments(sys.argv[1], sys.argv[2])
-    assert scientific_name == "Passer domesticus"
-    assert taxonomy_rank == "family"
+from scripts.getSpeciesRankName import  get_taxon_name,error_handle_taxon_name
 
 def test_get_taxon_name():
     scientific_name = "Passer Domesticus"
