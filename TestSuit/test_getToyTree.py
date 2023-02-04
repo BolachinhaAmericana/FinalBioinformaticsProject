f8927497a18cdfd9899fb67ff78a86d0ede0587d
marine.fournier2002 <marine.fournier2002@gmail.com>
2023-02-04 18:53:45 +0000

    added test_getToyTree

--- TestSuit/test_getToyTree.py
+++ TestSuit/test_getToyTree.py
@@ -1,14 +1,24 @@
-import sys
-from scripts.get_toy_tree import get_user_input, get_user_args
+from scripts.get_toy_tree import style_ml_tree, mb_tree_styler, write_to_pdf
+import toytree
+import toyplot.pdf
+import os
 
-def test_get_user_input():
-    sys.argv = ['scriptname', 'tree1', 'tree2']
-    MLTree, MrBayes = get_user_input(MLTree='', MrBayes='')
-    assert MLTree == 'tree1'
-    assert MrBayes == 'tree2'
+def test_style_ml_tree():
+    specie_name = "A"
+    tree = toytree.tree("(A:1,B:2);")
+    result = style_ml_tree(specie_name, tree)
+    assert isinstance(result, toyplot.canvas.Canvas)
 
-def test_get_user_args():
-    sys.argv = ['scriptname', 'tree1', 'tree2', 'species name']
-    specieName = get_user_args(specieName='')
-    assert specieName == 'species_name'
+def test_mb_tree_styler():
+    specie_name = "A"
+    tree = toytree.tree("(A:1,B:2);")
+    result = mb_tree_styler(specie_name, tree)
+    assert isinstance(result, toyplot.canvas.Canvas)
 
+def test_write_to_pdf():
+    ml_canvas = toyplot.canvas.Canvas()
+    mb_canvas = toyplot.canvas.Canvas()
+    write_to_pdf(ml_canvas, mb_canvas)
+    # Verifica se os arquivos PDF foram criados corretamente
+    assert os.path.isfile("tree-plot_bootstrap_ML.pdf")
+    assert os.path.isfile("tree-plot_MB.pdf")
