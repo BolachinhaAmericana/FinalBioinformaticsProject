37dade6bfec6e30fee6483a583555dd42b6415d1
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-02-06 20:29:44 +0000

    Try To Solve Path in CI

--- TestSuit/test_getSpeciesGeneList.py
+++ TestSuit/test_getSpeciesGeneList.py
@@ -4,7 +4,7 @@ from scripts.getSpeciesGeneList import env_setup
 import shutil
 
 def test_env_setup():
-    output_dir = 'TestSuit/GeneLists'
+    output_dir = '../TestSuit/GeneLists'
     env_setup(output_dir)
     assert os.path.exists(output_dir)
     shutil.rmtree(output_dir)
